c6a9b2e0b55d43ca9681ee3a5acccece03f9d084
Hans Ole Hatzel <hhatzel@gmail.com>
2021-06-02 10:51:00 +0200

    Expose important classes from __init__.py
    This way the external user needs to know less about the internal module
    structure.

---
+++ catma_gitlab/__init__.py
@@ -0,0 +1,7 @@
+from .tag_class import Tag
+from .tagset_class import Tagset
+from .annotation_class import Annotation
+from .project_class import CatmaProject
+from .property_class import Property
+from .text_class import Text
+from .annotation_collection_class import AnnotationCollection
--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setuptools.setup(
     name="catma-gitlab",
     version="0.0.1",
     author="Michael Vauth",
+    packages=setuptools.find_packages(),
     description="Load CATMA annotations from their git data",
     url="https://github.com/michaelvauth/catma_gitlab",
-    package_dir={},
     python_requires=">=3.5",
     install_requires=["pandas"]
 )
