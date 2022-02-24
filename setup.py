76f4a55ca6b978c0565164ebb245ebce5b185ec7
vauth <michael.vauth@gmx.de>
2022-02-24 10:40:53 +0100

    added backup directory to load from gitlab method

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ import setuptools
 
 setuptools.setup(
     name="gitma",
-    version="1.3.5",
+    version="1.3.6",
     author="Michael Vauth",
     packages=setuptools.find_packages(),
     description="Load CATMA annotations from their git data",
     url="https://github.com/forTEXT/gitma",
     python_requires=">=3.5",
-    install_requires=["pandas", "python-gitlab",
-                      "matplotlib", "plotly", "spacy", "tabulate"]
+    install_requires=[
+        "pandas", "python-gitlab", "pygit2", "plotly", "spacy", "tabulate"
+    ]
 )
