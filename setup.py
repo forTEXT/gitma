dad1af5bdc8e9121b41041005a456202a1185a83
vauth <michael.vauth@gmx.de>
2022-03-05 07:42:54 +0100

    v. 1.4.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="gitma",
-    version="1.4.8",
+    version="1.4.9",
     author="Michael Vauth",
     packages=setuptools.find_packages(),
     description="Load CATMA annotations from their Git data",
