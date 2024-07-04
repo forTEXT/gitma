0f5d341522fd310aa9f0cf512500a5e43b21a031
Malte Meister <malte.meister@gmx.net>
2024-07-04 11:12:40 +0200

    pin Python version in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     description="Load CATMA annotations from their Git data",
     url="https://github.com/forTEXT/gitma",
-    python_requires=">=3.7",
+    python_requires="==3.9.*",
     install_requires=[
         "cvxopt==1.2.7",
         "jupyter",
