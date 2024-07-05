171283dc9e0d87074fc16ce566893bf9ac1fd92c
Malte Meister <malte.meister@gmx.net>
2024-07-05 16:58:19 +0200

    v2.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="gitma",
-    version="2.0.2",
+    version="2.0.3",
     author="Michael Vauth",
     packages=setuptools.find_packages(),
     description="Load CATMA annotations from their Git data",
